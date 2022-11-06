83b0443ea204d27f21551f546d3f9c5cc62272e6
JarviZ <jarvux@gmail.com>
2022-11-05 19:27:04 -0500

    test duplicate code v2

--- src/restaurante/restaurante.service.ts
+++ src/restaurante/restaurante.service.ts
@@ -102,4 +102,30 @@ export class RestauranteService {
 
     await this.restauranteRepository.remove(restaurante);
   }
+  async delete3(id: string) {
+    const restaurante: RestauranteEntity =
+        await this.restauranteRepository.findOne({
+          where: { id },
+        });
+    if (!restaurante)
+      throw new BusinessLogicException(
+          'El restaurante con el id dado no existe',
+          BusinessError.NOT_FOUND,
+      );
+
+    await this.restauranteRepository.remove(restaurante);
+  }
+  async delete4(id: string) {
+    const restaurante: RestauranteEntity =
+        await this.restauranteRepository.findOne({
+          where: { id },
+        });
+    if (!restaurante)
+      throw new BusinessLogicException(
+          'El restaurante con el id dado no existe',
+          BusinessError.NOT_FOUND,
+      );
+
+    await this.restauranteRepository.remove(restaurante);
+  }
 }
